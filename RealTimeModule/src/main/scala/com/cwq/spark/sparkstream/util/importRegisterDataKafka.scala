7ae49631407c24eac08cd42bac7a7b54fcad309f
cwq <2430218212@qq.com>
2019-11-06 19:04:28 +0800

    添加备注

--- RealTimeModule/src/main/scala/com/cwq/spark/sparkstream/util/importRegisterDataKafka.scala
+++ RealTimeModule/src/main/scala/com/cwq/spark/sparkstream/util/importRegisterDataKafka.scala
@@ -17,20 +17,23 @@ object importRegisterDataKafka {
       ssc.textFile("D:\\My_pro\\IDEA_Project\\SparkProDemo\\RealTimeModule\\src\\main\\resources\\reginster.txt")
         .foreachPartition(partition => {
           val props = new Properties()
-          props.put("bootstrap.servers", "hadoop200:9092,hadoop201:9092,hadoop202:9092")
-          props.put("acks", "1")
-          props.put("batch.size", "16384")
-          props.put("linger.ms", "10")
-          props.put("buffer.memory", "33554432")
-          props.put("key.serializer",
-            "org.apache.kafka.common.serialization.StringSerializer")
-          props.put("value.serializer",
-            "org.apache.kafka.common.serialization.StringSerializer")
+          props.put("bootstrap.servers", "hadoop200:9092,hadoop201:9092,hadoop202:9092") //kafka集群地址
+          props.put("acks", "1")//ack应答机制 0、1、-1(all)
+          props.put("batch.size", "16384")//批次大小
+          props.put("linger.ms", "10")//等待时间
+          props.put("buffer.memory", "33554432")//缓存区大小
+          props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer")//key序列化
+          props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer")//value序列化
+
           val producer = new KafkaProducer[String, String](props)
+          //将分区内的数据写入kafka
           partition.foreach(item => {
+            //将数据封装成ProducerRecord
             val msg = new ProducerRecord[String, String]("test",item)
+            //将数据交给生产者
             producer.send(msg)
           })
+
           producer.flush()
           producer.close()
         })
